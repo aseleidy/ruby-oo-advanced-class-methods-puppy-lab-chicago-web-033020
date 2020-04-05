8b3131560d3222847c66867ce499436cd8b028ec
Ashley Etemadi <ash.etemadi@gmail.com>
2020-04-05 16:50:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,17 @@
 # Add your code here
 
 class Dog 
+  attr_reader :name 
   
   @@all = []
 
   def initialize(name)
     @name = name
     self.class.all << self 
+  end
+  
+  def self.all 
+    
   end
 
 end
